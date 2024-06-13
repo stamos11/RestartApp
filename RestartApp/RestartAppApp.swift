ce7038002abb97b2c1ceae2c202ecb18b884b5e2
Nicholas Stamoulis <135749660+stamos11@users.noreply.github.com>
2024-06-13 15:01:52 +0300

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by stamoulis nikolaos on 13/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
